2dbbc7618d7b1a6d3d0fb0b2088cd4e081a7cd7e
Lidia Makarchuk <lidiamakarcuk9@gmail.com>
2022-12-30 22:50:50 +0200

    Add project files.

---
+++ laba6/ChooseDifficulty.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace laba6
+{
+    partial class ChooseDifficulty
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMedium = new System.Windows.Forms.Button();
+            this.btnEasy = new System.Windows.Forms.Button();
+            this.btnMenu = new System.Windows.Forms.Button();
+            this.btnHard = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnMedium
+            // 
+            this.btnMedium.Font = new System.Drawing.Font("SimSun-ExtB", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnMedium.Location = new System.Drawing.Point(562, 285);
+            this.btnMedium.Name = "btnMedium";
+            this.btnMedium.Size = new System.Drawing.Size(245, 109);
+            this.btnMedium.TabIndex = 3;
+            this.btnMedium.Text = "Medium";
+            this.btnMedium.UseVisualStyleBackColor = true;
+            // 
+            // btnEasy
+            // 
+            this.btnEasy.Font = new System.Drawing.Font("SimSun-ExtB", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnEasy.Location = new System.Drawing.Point(562, 152);
+            this.btnEasy.Name = "btnEasy";
+            this.btnEasy.Size = new System.Drawing.Size(245, 109);
+            this.btnEasy.TabIndex = 2;
+            this.btnEasy.Text = "Easy";
+            this.btnEasy.UseVisualStyleBackColor = true;
+            // 
+            // btnMenu
+            // 
+            this.btnMenu.Font = new System.Drawing.Font("SimSun-ExtB", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnMenu.Location = new System.Drawing.Point(562, 546);
+            this.btnMenu.Name = "btnMenu";
+            this.btnMenu.Size = new System.Drawing.Size(245, 109);
+            this.btnMenu.TabIndex = 5;
+            this.btnMenu.Text = "Back To Menu";
+            this.btnMenu.UseVisualStyleBackColor = true;
+            // 
+            // btnHard
+            // 
+            this.btnHard.Font = new System.Drawing.Font("SimSun-ExtB", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnHard.Location = new System.Drawing.Point(562, 413);
+            this.btnHard.Name = "btnHard";
+            this.btnHard.Size = new System.Drawing.Size(245, 109);
+            this.btnHard.TabIndex = 4;
+            this.btnHard.Text = "Hard";
+            this.btnHard.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(522, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(335, 36);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "CHOOSE DIFFICULTY";
+            // 
+            // ChooseDifficulty
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1364, 691);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnMenu);
+            this.Controls.Add(this.btnHard);
+            this.Controls.Add(this.btnMedium);
+            this.Controls.Add(this.btnEasy);
+            this.Name = "ChooseDifficulty";
+            this.Text = "ChooseDifficulty";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnMedium;
+        private Button btnEasy;
+        private Button btnMenu;
+        private Button btnHard;
+        private Label label1;
+    }
+}
---
+++ laba6/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace laba6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStartGame = new System.Windows.Forms.Button();
+            this.btnQuite = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStartGame
+            // 
+            this.btnStartGame.Font = new System.Drawing.Font("SimSun-ExtB", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnStartGame.Location = new System.Drawing.Point(556, 145);
+            this.btnStartGame.Name = "btnStartGame";
+            this.btnStartGame.Size = new System.Drawing.Size(245, 109);
+            this.btnStartGame.TabIndex = 0;
+            this.btnStartGame.Text = "Start Game";
+            this.btnStartGame.UseVisualStyleBackColor = true;
+            // 
+            // btnQuite
+            // 
+            this.btnQuite.Font = new System.Drawing.Font("SimSun-ExtB", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnQuite.Location = new System.Drawing.Point(556, 320);
+            this.btnQuite.Name = "btnQuite";
+            this.btnQuite.Size = new System.Drawing.Size(245, 109);
+            this.btnQuite.TabIndex = 1;
+            this.btnQuite.Text = "Quite";
+            this.btnQuite.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1365, 662);
+            this.Controls.Add(this.btnQuite);
+            this.Controls.Add(this.btnStartGame);
+            this.Name = "Form1";
+            this.Text = "Skrap";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button btnStartGame;
+        private Button btnQuite;
+    }
+}
